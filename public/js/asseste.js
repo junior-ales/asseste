33ab86799106d5590f86823f266390859b71caa1
Junior Ales <edilson.ales.jr@gmail.com>
2014-02-14 01:13:11 -0200

    Criando estrutura de páginas e navegação do site

--- public/js/asseste.js
+++ public/js/asseste.js
@@ -1,1 +1,12 @@
-$(document).ready(function() { });
+var activeLink = {
+  init: function() {
+    var uri = window.location.pathname.slice(1);
+    $('[data-nav-link]').each(function(i, elem) {
+      if($(elem).data('nav-link') === uri) { $(elem).addClass('active'); }
+    });
+  }
+};
+
+$(document).ready(function() {
+  activeLink.init();
+});
